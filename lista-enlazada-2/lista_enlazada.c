12163e6c7cb653e4802499f63e05921f5091cbb3
Rafael305 <fraq86@gmail.com>
2026-06-18 15:03:00 -0300

    funcion para quitar un nodo puntual

--- lista-enlazada-2/lista_enlazada.c
+++ lista-enlazada-2/lista_enlazada.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
 typedef struct Nodo{
     int valor;
@@ -12,6 +13,8 @@ void agregarNodoAlPrincipio(nodo **, int);
 // void agregarNodo2(nodo *, int);
 void quitarNodoAlPrincipio(nodo **);
 void quitarUltimoNodo(nodo *);
+int quitarUnNodoPuntual(nodo **, int);
+
 
 int main(){
     nodo * head = NULL;
@@ -46,6 +49,9 @@ int main(){
     imprimirLista(head);
 
     quitarUltimoNodo(head);
+    imprimirLista(head);
+
+    quitarUnNodoPuntual(&head, 3);
     imprimirLista(head);
     
     return 0;
@@ -121,4 +127,42 @@ void quitarUltimoNodo(nodo * head){
 
     free(*actual);
     * actual = NULL;
+}
+
+int quitarUnNodoPuntual(nodo ** punteroAHead, int valorDelNodo){ 
+    nodo * segundoPuntero = (*punteroAHead)->proximo;
+    bool valorPresente = false;
+
+    if((*punteroAHead)->valor == valorDelNodo){
+        free(*punteroAHead);
+        *punteroAHead = segundoPuntero;
+        return 0;
+    }
+    
+    while(segundoPuntero->proximo != NULL){
+/*
+        if((*punteroAHead)->valor == valorDelNodo){
+            valorPresente = true;
+        }*/
+
+        punteroAHead = &((*punteroAHead)->proximo);
+        segundoPuntero = segundoPuntero->proximo;
+
+        if((*punteroAHead)->valor == valorDelNodo){
+            free(*punteroAHead);
+            *punteroAHead = segundoPuntero;
+            valorPresente = true;
+            break;
+        }
+    }
+
+    if(!valorPresente){
+        printf("El nodo solicitado no se encuentra en la lista\n");
+        return 1;
+    }
+    /*
+    free(*punteroAHead);
+    *punteroAHead = segundoPuntero;*/
+
+    return 0;
 }
